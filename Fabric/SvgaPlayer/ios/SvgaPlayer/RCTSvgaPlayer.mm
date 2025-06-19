bb0fc61f9450e37f038c41fc6d123837b773835d
yanruifeng <yanruifeng@syriusrobotics.com>
2025-06-19 13:16:43 +0800

    feat:更新

--- Fabric/SvgaPlayer/ios/SvgaPlayer/RCTSvgaPlayer.mm
+++ Fabric/SvgaPlayer/ios/SvgaPlayer/RCTSvgaPlayer.mm
@@ -24,8 +24,23 @@ using namespace facebook::react;
 }
   
 
--(instancetype)init{
-    if(self = [super init]) {
+//-(instancetype)init{
+//    if(self = [super init]) {
+//      _aPlayer = [[SVGAPlayer alloc] init];
+//      _aPlayer.delegate = self;
+//      // _aPlayer.loops = 1;
+//      // _aPlayer.clearsAfterStop = YES;
+//      _aPlayer.clipsToBounds = NO;
+//      _aPlayer.contentMode = UIViewContentModeScaleAspectFit;
+//      [self addSubview:_aPlayer];
+//    }
+//    return self;
+//  }
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        static const auto defaultProps = std::make_shared<const SvgaPlayerViewProps>();
+        _props = defaultProps;
       _aPlayer = [[SVGAPlayer alloc] init];
       _aPlayer.delegate = self;
       // _aPlayer.loops = 1;
@@ -35,7 +50,7 @@ using namespace facebook::react;
       [self addSubview:_aPlayer];
     }
     return self;
-  }
+}
 
 -(void)layoutSubviews
 {
